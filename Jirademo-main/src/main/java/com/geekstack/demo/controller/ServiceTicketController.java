db1932464d617ffb14f846d230c69d70e2102326
kalpeshVivanet <100676967+kalpeshVivanet@users.noreply.github.com>
2023-02-14 18:43:53 +0530

    created suuccessfullY (#1)

--- Jirademo-main/src/main/java/com/geekstack/demo/controller/ServiceTicketController.java
+++ Jirademo-main/src/main/java/com/geekstack/demo/controller/ServiceTicketController.java
@@ -1,27 +1,34 @@
 package com.geekstack.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.geekstack.demo.model.JiraPayload;
+// import com.geekstack.demo.model.JiraPayload;
 import com.geekstack.demo.service.JiraTicketService;
 
 @RestController
 public class ServiceTicketController {
 
+	private JiraTicketService jiraTicketService;
+
 	@Autowired
 	public ServiceTicketController(JiraTicketService jiraTicketService) {
+		this.jiraTicketService = jiraTicketService;
 	}
 
 	@PostMapping("/ticket.create")
-	public ResponseEntity<String> createJiraTicket(@RequestBody JiraPayload jiraPayload) {
-
-		return new ResponseEntity<String>(HttpStatus.CREATED);
-
+	public String createJiraTicket() {
+		try {
+			String response = jiraTicketService.createJiraTicket();
+			return response;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ("Unable to process your requesttt");
+		}
 	}
 
 }
