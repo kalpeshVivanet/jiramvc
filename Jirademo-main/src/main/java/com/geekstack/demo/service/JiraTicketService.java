aed5b4422f13bb2fbe8eb5d46328d43692267a1b
kalpesh <kalpeshchouhan@vivanet.in>
2023-02-14 18:42:15 +0530

    created suuccessfullY

--- Jirademo-main/src/main/java/com/geekstack/demo/service/JiraTicketService.java
+++ Jirademo-main/src/main/java/com/geekstack/demo/service/JiraTicketService.java
@@ -13,16 +13,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.geekstack.demo.model.JiraPayload;
+// import com.geekstack.demo.model.JiraPayload;
+
+// import com.geekstack.demo.model.JiraPayload;
 
 @Service
 public class JiraTicketService {
-	private HttpHeaders httpHeaders;
+	// private HttpHeaders httpHeaders;
 	private RestTemplate restTemplate;
 	// @Value("${jira.user-name}")
 	private String username = "kalpeshchouhan2@gmail.com";
 	// @Value("${jira.secret}")
-	private String secret = "ap6mLN3z29cyyoS5fbAi0F20";
+	private String secret = "ATATT3xFfGF0m4WrotWnss9oBiM9N9ljqDCN9Faz-84zhLKW-USyQlTrS_9Y7FSLrQ3b6q7giG21LIvg2n8IKx2rV41QqpCvKoM2RU6yNLNJ65IE1Zs3s1ij8tYu86M-veLVCjVWO1Xews5U7dlfjj529TYbzL99qxof99OoQJNEO-Rv6X0mAfo=F597A615";
 	// @Value("${jira.base-url}")
 	private String baseUrl = "https://mykalpeshchouhan.atlassian.net";
 	// @Value("${jira.ticket-url}")
@@ -33,18 +35,26 @@ public class JiraTicketService {
 		this.restTemplate = restTemplate;
 	}
 
-	public ResponseEntity createJiraTicket(JiraPayload jiraPayload) {
+	public String createJiraTicket() {
+		System.out.println("////////////////");
 		System.out.println(baseUrl.concat(ticketCreationUrl));
 		System.out.println(getHeaders());
 		///////
-		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
 
-		HttpEntity<JiraPayload> requestEntity = new HttpEntity<JiraPayload>(jiraPayload,
+		String createIssueJSON = createCreateIssueJSON("BT", "summary",
+				"description", "Bug");
+		// httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+		HttpEntity<String> requestEntity = new HttpEntity<String>(createIssueJSON,
 				getHeaders());
 		System.out.println(requestEntity);
-		return restTemplate.exchange(baseUrl.concat(ticketCreationUrl), HttpMethod.POST, requestEntity,
+		ResponseEntity<String> response = restTemplate.exchange(baseUrl.concat(ticketCreationUrl),
+				HttpMethod.POST, requestEntity,
 				String.class);
-
+		if (response != null) {
+			return response.getBody();
+		}
+		return null;
 		/////////
 		// ResponseEntity<String> response =
 		///////// restTemplate.exchange(baseUrl.concat(ticketCreationUrl),
@@ -63,7 +73,18 @@ public class JiraTicketService {
 		String authHeader = "Basic " + new String(encodedAuth);
 		// System.out.println(authHeader);
 		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
 		headers.set("Authorization", authHeader);
 		return headers;
 	}
+
+	public String createCreateIssueJSON(String key, String summary, String description, String issueType) {
+		String createIssueJSON = "{\"fields\":{\"project\":{\"key\":\"$KEY\"},\"summary\":\"$SUMMARY\",\"description\":\"$DESCRIPTION\",\"issuetype\":{\"name\":\"$ISSUETYPE\"}}}";
+
+		createIssueJSON = createIssueJSON.replace("$KEY", key);
+		createIssueJSON = createIssueJSON.replace("$SUMMARY", summary);
+		createIssueJSON = createIssueJSON.replace("$DESCRIPTION", description);
+		return createIssueJSON.replace("$ISSUETYPE", issueType);
+	}
+
 }
